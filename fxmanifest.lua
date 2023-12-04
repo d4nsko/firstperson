c875111fa5134f9b40326791fef766a6a3e5a106
d4nsko <76801103+d4nsko@users.noreply.github.com>
2023-12-04 09:47:37 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version      'cerulean'
+game 		    'gta5'
+lua54		    'yes'
+
+author          'd4nsko'
+description     'A resource that forces you to shoot & aim in First Person View.'
+
+client_scripts {
+    'client.lua',
+}
+
